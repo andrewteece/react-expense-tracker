6ab7b67ea6b2fbdbe59be20342a5a9ca125230ec
andrewteece <andrewteece@gmail.com>
2025-06-01 09:33:04 -0500

    setup initial array for expenses

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
 import ExpenseForm from './components/ExpenseForm';
 import ExpenseList from './components/ExpenseList';
 import './App.css';
+import { useState } from 'react';
+
+const initialExpenses = [
+  {
+    id: 1,
+    title: 'Groceries',
+    amount: 60,
+    date: '2025-05-01',
+  },
+  {
+    id: 2,
+    title: 'House Rent',
+    amount: 600,
+    date: '2025-05-01',
+  },
+  {
+    id: 3,
+    title: 'Internet Bill',
+    amount: 60,
+    date: '2025-04-28',
+  },
+];
 
 function App() {
+  const [expenses, setExpenses] = useState(initialExpenses);
+
   return (
     <div className='tracker'>
       <h1>React Expense Tracker</h1>
