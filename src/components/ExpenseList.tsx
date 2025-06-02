51c6bf00162e8e05c798725c4376ac92a4bc3278
andrewteece <andrewteece@gmail.com>
2025-06-02 11:07:49 -0500

    update eslint and refactor ExpenseList

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -1,9 +1,10 @@
 function ExpenseList(props) {
-  // console.log(props);
+  console.log(props);
+
   return (
     <ul className='expense-list'>
-      {props.expenses.map((expense) => {
-        ;<li key={expense.id} className='expense-item'>
+      {props.expenses.map((expense) => (
+        <li key={expense.id} className='expense-item'>
           <div>
             <h3>{expense.title}</h3>
             <p>
@@ -12,9 +13,9 @@ function ExpenseList(props) {
           </div>
           <button>🗑️</button>
         </li>
-      })}
+      ))}
     </ul>
-  )
+  );
 }
 
-export default ExpenseList
+export default ExpenseList;
