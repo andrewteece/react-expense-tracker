30a40d65ab4ba1fda666828a0b8a66352f6101c8
andrewteece <andrewteece@gmail.com>
2025-06-02 12:52:37 -0500

    add events

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,9 +1,35 @@
+import { useState } from 'react';
+
 function ExpenseForm() {
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState(0);
+  const [date, setDate] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefualt();
+    if (!title || !amount || !date) return;
+  }
+
   return (
-    <form className='expense-form'>
-      <input type='text' placeholder='Expense Title' />
-      <input type='number' min={0} placeholder='Amount' />
-      <input type='date' />
+    <form className='expense-form' onSubmit={handleSubmit}>
+      <input
+        type='text'
+        placeholder='Expense Title'
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type='number'
+        min={0}
+        placeholder='Amount'
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+      />
+      <input
+        type='date'
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+      />
       <button>Add Expense</button>
     </form>
   );
